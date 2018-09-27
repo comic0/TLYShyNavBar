e1d10ebea22cdd26841a5e6061ce5738d8f96fa2
Alexandre CHASTAN <alexandre.chastan@gmail.com>
2018-09-27 19:11:16 +0200

    Update TLYShyStatusBarController.m

--- TLYShyNavBar/ShyControllers/TLYShyStatusBarController.m
+++ TLYShyNavBar/ShyControllers/TLYShyStatusBarController.m
@@ -14,30 +14,12 @@
 
 static inline CGFloat AACStatusBarHeight(UIViewController *viewController)
 {
-    if ([UIApplication sharedApplication].statusBarHidden)
-    {
-        return 0.f;
-    }
-    
-    // Modal views do not overlap the status bar, so no allowance need be made for it
-    if (viewController.presentingViewController != nil)
-    {
-        return 0.f;
-    }
-
     CGSize  statusBarSize = [UIApplication sharedApplication].statusBarFrame.size;
     CGFloat statusBarHeight = MIN(statusBarSize.width, statusBarSize.height);
     
     UIView *view = viewController.view;
     CGRect frame = [view.superview convertRect:view.frame toView:view.window];
-    
-    BOOL viewOverlapsStatusBar = frame.origin.y < statusBarHeight;
-    
-    if (!viewOverlapsStatusBar)
-    {
-        return 0.f;
-    }
-    
+        
     return statusBarHeight;
 }
 
@@ -46,14 +28,15 @@ static inline CGFloat AACStatusBarHeight(UIViewController *viewController)
 
 - (CGFloat)_statusBarHeight
 {
+    CGFloat statusBarDefaultHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
     CGFloat statusBarHeight = AACStatusBarHeight(self.viewController);
     /* The standard status bar is 20 pixels. The navigation bar extends 20 pixels up so it is overlapped by the status bar.
      * When there is a larger than 20 pixel status bar (e.g. a phone call is in progress or GPS is active), the center needs
      * to shift up 20 pixels to avoid this 'dead space' being visible above the usual nav bar.
      */
-    if (statusBarHeight > 20)
+    if (statusBarHeight > statusBarDefaultHeight)
     {
-        statusBarHeight -= 20;
+        statusBarHeight -= statusBarDefaultHeight;
     }
     
     return statusBarHeight;
